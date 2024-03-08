64c6333dcb23edaf32867a98401765ddcc587773
hornfrog <hmsakbkw@gmail.com>
2024-03-08 17:14:49 +0900

    名前、自己紹介保存成功

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,18 +10,14 @@ class ProfilesController < ApplicationController
 
   def update
     @user = current_user
-    if @user.update(profiles_params)
+    if @user.update(params.require(:user).permit(:username, :profile))
       flash[:notice] = "ユーザー情報を編集しました"
-      redirect_to :profile_path
+      redirect_to user_profile_path(@user.profile)
     else
       render :edit
     end  
   end
 
-  private
 
- def profiles_params
-  params.require(:profile).permit(:name, :profile)
- end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,10 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    
   end
 end
